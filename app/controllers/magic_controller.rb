1dee506494308ae5808249f9731fca15f0fdbd33
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-10-20 12:47:16 +0900

    add: 魔法使いからのメッセージを受け取ることに成功

--- app/controllers/magic_controller.rb
+++ app/controllers/magic_controller.rb
@@ -3,5 +3,38 @@ class MagicController < ApplicationController
   end
 
   def create
+    my_til = params[:my_til]
+
+    prompt_template = Rails.application.credentials.gemini[:prompt]
+
+    prompt = prompt_template.sub("{my_til}", my_til)
+
+    begin
+      result = GEMINI_CLIENT.stream_generate_content(
+        { contents: { role: "user", parts: { text: prompt } } },
+      )
+      # puts "==================="
+      # puts result
+      # puts "==================="
+
+      raw_response = result.map { |res| res.dig("candidates", 0, "content", "parts", 0, "text") }.join
+
+      cleaned_json_string = raw_response.gsub(/```(json)?/, "").strip
+
+      # puts "~~~~~~~~~~~~~~~~~~~"
+      # puts cleaned_json_string
+      # puts "~~~~~~~~~~~~~~~~~~~"
+
+      @magic = JSON.parse(cleaned_json_string)
+
+      # puts "-------------------"
+      # puts @magic
+      # puts "-------------------"
+
+    rescue Gemini::Errors::RequestError => e
+      @error_message = "🧙‍♀️「魔法がうまくかからないようです⋯。」: #{e.message}"
+    rescue JSON::ParserError => e
+      @error_message = "🧙‍♀️「私でも読めない魔法が⋯。もう一度お試しいただけますでしょうか。」#{e.message}"
+    end
   end
 end
