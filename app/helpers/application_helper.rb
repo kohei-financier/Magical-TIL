f90ef97212182217386c5f69066ba31d1953bfb3
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-10-26 19:04:57 +0900

    fix: lint修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,26 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: 'Magical TIL',
-      title: '「今日の学び」を「新しい魔法」を覚える楽しみに。',
-      description: '「今日の学び（TIL）」を分析して、新しい魔法を授けるサービスです'
+      site: "Magical TIL",
+      title: "「今日の学び」を「新しい魔法」を覚える楽しみに。",
+      description: "「今日の学び（TIL）」を分析して、新しい魔法を授けるサービスです",
       reverse: true,
-      charset: 'utf-8',
-      keywords: 'rails,TIL,魔法',
+      charset: "utf-8",
+      keywords: "rails,TIL,魔法",
       canonical: "https://magical-til.onrender.com/",
-      separator: '|',
-      og:{
+      separator: "|",
+      og: {
         site_name: :site,
         title: :title,
         description: :description,
-        type: 'website',
+        type: "website",
         url: "https://magical-til.onrender.com/",
-        image: image_url('ogp.png'),
-        local: 'ja-JP'
+        image: image_url("ogp.png"),
+        local: "ja-JP"
       },
       twitter: {
-        card: 'summary',
-        image: image_url('ogp.png')
+        card: "summary",
+        image: image_url("ogp.png")
       }
     }
   end
