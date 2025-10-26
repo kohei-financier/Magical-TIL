c9acab313ce5703279fac225baf393dd79a700e2
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-10-26 18:58:55 +0900

    add: ogp画像を追加

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem "thruster", require: false
 # Gemini
 gem "gemini-ai", "~> 4.3.0"
 
+# ogp
+gem "meta-tags"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'Magical TIL',
+      title: '「今日の学び」を「新しい魔法」を覚える楽しみに。',
+      reverse: true,
+      charset: 'utf-8',
+      keywords: 'rails,TIL,魔法',
+      canonical: "https://magical-til.onrender.com/",
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: "https://magical-til.onrender.com/",
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
