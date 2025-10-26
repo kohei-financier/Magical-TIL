3713ecbfaf1e4109ee935b8b3dac27ffe8adb06e
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-10-26 15:33:12 +0900

    add: クリップボードにコピーされたフィードバックを追加

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -9,19 +9,11 @@ export default class extends Controller {
 
     try {
       await navigator.clipboard.writeText(textToCopy);
-      this.showFeedback();
+      alert("クリップボードにコピーしました！");
 
     } catch (error) {
       console.error("クリップボードへのコピーに失敗しました: ", error);
       alert("コピーに失敗しました。");
     }
-  }
-
-  showFeedback() {
-    this.feedbackTarget.classList.remove("opacity-0");
-
-    setTimeout(() => {
-      this.feedbackTarget.classList.add("opacity-0");
-    }, 2000);
   }
 }
